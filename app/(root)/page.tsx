d4a6f4e90d71b095b3b700380b49add8105a1004
Pratham Handique <pa2289@srmist.edu.in>
2024-11-19 13:50:49 +0530

    seach functionaity added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import SearchForm from "../components/SearchForm";
+
+export default async function Home({searchParams}: {searchParams: Promise<{ query?: string }>;}) 
+{
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Share your myfarm story,
+          <br />
+          connect with other myfarmers
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Join the community of myfarmers and share your story with
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
